23cca08b0aa83f409e877158044d39782a450bcb
犬走椛 <moemoechu@yeah.net>
2025-08-19 13:17:27 +0800

    add translate.

--- src/translations.ts
+++ src/translations.ts
@@ -6,7 +6,31 @@ export const translations: [string, PluginTranslations][] = [
     {
       Charset: "字符集",
       Common: "常用",
-      CodePage: "代码页",
+      Misc: "杂项",
+    },
+  ],
+  [
+    "zh-TW",
+    {
+      Charset: "字元集",
+      Common: "常用",
+      Misc: "雜項",
+    },
+  ],
+  [
+    "ja-JP",
+    {
+      Charset: "文字コード",
+      Common: "一般的な",
+      Misc: "その他の",
+    },
+  ],
+  [
+    "ko-KR",
+    {
+      Charset: "문자 집합",
+      Common: "일반적인",
+      Misc: "잡다한",
     },
   ],
 ];
